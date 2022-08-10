3b9905ef441d17161f27137fb95e80c777ce6c39
einatsof <39820834+einatsof@users.noreply.github.com>
2022-08-10 08:06:38 +0300

    Update index.js

--- rotter-news-headlines-telegram-bot/index.js
+++ rotter-news-headlines-telegram-bot/index.js
@@ -9,10 +9,10 @@ const bot = new TelegramBot(token, {polling: true});
 // chat id number
 const chatId = XXXXXXXXX;
 const interval = 60 * 1000; // 1 minute polling interval
-let date;
+let time;
 
 setInterval(() => {
-  if (date) {
+  if (time) {
     let req = https.get("https://rotter.net/rss/rotternews.xml", (res) => {
       res.setEncoding('binary'); 
       let resBuf = new Buffer.alloc(102400);
@@ -24,30 +24,30 @@ setInterval(() => {
         const xml = iconv.decode(resBuf, 'win1255');
         const regex = /<item>(.*?)<\/item>/g;
         const results = Array.from(xml.matchAll(regex), x=>x[1]);
-        const now = Date.parse(results[0].match(/<pubDate>(.*?)<\/pubDate>/g)[0].replace(/<\/?pubDate>/g,'')) / 1000;
-        if (date < now){
+        const last = Date.parse(results[0].match(/<pubDate>(.*?)<\/pubDate>/g)[0].replace(/<\/?pubDate>/g,'')) / 1000;
+        if (time < last){
           results.reverse(); // start from older news
           for (const result of results) {
             const matchTitle = result.match(/<title>(.*?)<\/title>/g);
             const title = matchTitle[0].replace(/<\/?title>/g,'').replace('&amp;', '').replace('#1524;', '"').replace('&#1524;', '"');
-            const matchTime = result.match(/<pubDate>(.*?)<\/pubDate>/g);
-            const time = matchTime[0].replace(/<\/?pubDate>/g,'');
+            const matchDate = result.match(/<pubDate>(.*?)<\/pubDate>/g);
+            const date = matchDate[0].replace(/<\/?pubDate>/g,'');
             const matchLink = result.match(/<link>(.*?)<\/link>/g);
             const articleLink = matchLink[0].replace(/<\/?link>/g,'');
-            const timestamp = Date.parse(time) / 1000;
-            if (now >= timestamp && timestamp > date){
+            const timestamp = Date.parse(date) / 1000;
+            if (last >= timestamp && timestamp > time){
               displayTime = new Date(timestamp * 1000);
               let h = displayTime.getHours();
               let m = displayTime.getMinutes();
               bot.sendMessage(chatId, (h < 10 ? '0' + h : h) + ":" + (m < 10 ? '0' + m : m) + " - <a href='" + articleLink + "'>" + title + "</a>", {parse_mode : "HTML"});
             }
           }
-          date = now;
+          time = last;
         }
       });
     });
   } else {
-    date = Date.now() / 1000;
+    time = Date.now() / 1000;
   }
 }, interval);
 
